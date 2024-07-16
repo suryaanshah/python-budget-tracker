e4c5b5b03c61552f35943c6b7c4c81e37f2331bf
suryaanshchawla <suryaanshchawla@gmail.com>
2024-07-16 09:03:09 +0530

    asks for the expense

--- tracker.py
+++ tracker.py
@@ -1,4 +1,17 @@
-import pandas as pd
-import csv
-for datetime import datetime
+
+"""
+What it does:
+1. help you track: expenses (E), income (I)
+2. generates your balance sheet
+3. ask your assets: what do you own
+4. ask your liabilities: obligation to someone else
+5. tell your equity/net-worth: A=L+E
+"""
+
+track=input("Is it an expense (E) or an income (I)? ");
+
+if track == "E":
+    expense=int(input("please enter the amount of expense: "));
+    date=input("Please enter the date: ");
+
 
