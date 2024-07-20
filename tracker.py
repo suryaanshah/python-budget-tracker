65fc68a7f0e25b76ba4f9b377a7faa697138b3ed
suryaanshchawla <suryaanshchawla@gmail.com>
2024-07-20 12:26:53 +0530

    1. fix date enterring issue 2. change recurrsion to while loop 3. make csv global and add types

--- tracker.py
+++ tracker.py
@@ -1,71 +1,60 @@
 from datetime import datetime
 import pandas as pd
-import csv
 
 
-
-
+df = pd.DataFrame(columns=["date", "amount", "type"])
 
 def main():
-    todo=input("What do want to do?\n 1. record a statement (R)\n 2. show the balance-sheet (B)\n 3. tutorial (T)\nYour Answer: ")
-    if todo.upper()=='R':
+    todo = input("What do want to do?\n 1. record a statement (R)\n 2. show the balance-sheet (B)\n 3. tutorial (T)\nYour Answer: ")
+    if todo.upper() == 'R':
         record()
-
-    # elif todo.upper()=='B':
+    # elif todo.upper() == 'B':
     #     balance()
-    # elif todo.upper()=='T':
+    # elif todo.upper() == 'T':
     #     tutorial()
-
     else:
         main()
 
-
 def record():
-
-    track=input("Is it an expense (E) or an income (I)? ")
+    track = input("Is it an expense (E) or an income (I)? ")
 
     if track.upper() == "E":
-        trackexpense()
-        trackdate()
-
+        amount = trackexpense()
+        date = trackdate()
+        df.loc[len(df)] = [date, amount, "expense"]
+        df.to_csv("track.csv", index=False)
     elif track.upper() == "I":
-        trackincome()
-        trackdate()
-
+        amount = trackincome()
+        date = trackdate()
+        df.loc[len(df)] = [date, amount, "income"]
+        df.to_csv("track.csv", index=False)
     else:
         record()
 
-
-#def balance():
-
-
-
 def trackexpense():
-    try:
-        expense=int(input("enter the expense: "))
-    except ValueError:
-        print("Not an integer")
-        trackexpense()
-
+    while True:
+        try:
+            expense = int(input("Enter the expense: "))
+            return expense
+        except ValueError:
+            print("Not an integer, please try again.")
 
 def trackincome():
-    try:
-        income=int(input("enter the income: "))
-    except ValueError:
-        print("Not an integer")
-        trackincome()
-
+    while True:
+        try:
+            income = int(input("Enter the income: "))
+            return income
+        except ValueError:
+            print("Not an integer, please try again.")
 
 def trackdate():
-    try:
-        date=input("enter the date (DD-MM-YY): ")
-        date_object = datetime.datetime.strptime(date, "%d-%m-%Y").date()
-    except:
-        print("Invalid Date Format")
-        trackdate()
-
-
-
+    while True:
+        try:
+            date_str = input("Enter the date (DD-MM-YYYY): ")
+            date_object = datetime.strptime(date_str, "%d-%m-%y").date()
+            return date_object
+        except ValueError:
+            print("Invalid Date Format, please try again.")
 
 main()
 
